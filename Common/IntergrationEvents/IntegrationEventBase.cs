5e4d691970870742864b3d2957857b1ae493a35d
Md Faruk Hossen Khan <MdFarukHossen.Khan@bakerhughes.com>
2024-12-10 19:56:40 +0100

    Decoupled the integration events to share among the producer and Consumer

---
+++ Common/IntergrationEvents/IntegrationEventBase.cs
@@ -0,0 +1,12 @@
+﻿namespace IntergrationEvents
+{
+    public class IntegrationEventBase
+    {
+        public DateTime CreateDateTime{ get; protected set; }
+
+        protected IntegrationEventBase()
+        {
+            CreateDateTime = DateTime.UtcNow;
+        }
+    }
+}
---
+++ Common/IntergrationEvents/PositionCreatedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace IntergrationEvents
+{
+    public class PositionCreatedIntegrationEvent : IntegrationEventBase
+    {
+        public Guid Id{ get; }
+        public double Latitude{ get; }
+        public double Longitude{ get; }
+        public double Height{ get; }
+
+        public PositionCreatedIntegrationEvent(Guid id, double latitude, double longitude, double height)
+        {
+            Id = id;
+            Latitude = latitude;
+            Longitude = longitude;
+            Height = height;
+        }
+    }
+}
